f8f99fcc4d6207de7caacbb25d9c9122a69f0053
Tung Pham <tung.42@gmail.com>
2025-12-27 17:22:48 +0700

    Update speech.ts

--- src/utils/speech.ts
+++ src/utils/speech.ts
@@ -8,11 +8,6 @@ export const speak = async (text: string) => {
       `https://googlespeak.netlify.app/api/tts?text=${text}&lang=ja`
     );
 
-    if (response.data && response.data.audioContent) {
-      const audio = new Audio(response.data.audioContent);
-    }
-    const url = `/.netlify/functions/tts?text=${encodeURIComponent(text)}`;
-
     // Create a new Audio object and play it
     const audio = new Audio(response.data.audioContent);
 
