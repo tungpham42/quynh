35c26fb7e63d5b70d29289d901a988967a7ac416
Tung Pham <tung.42@gmail.com>
2025-12-27 17:15:48 +0700

    TTS

--- src/utils/speech.ts
+++ src/utils/speech.ts
@@ -1,11 +1,20 @@
-export const speak = (text: string) => {
+import axios from "axios";
+
+export const speak = async (text: string) => {
   if (!text) return;
 
   try {
+    const response = await axios.get(
+      `https://googlespeak.netlify.app/api/tts?text=${text}&lang=ja`
+    );
+
+    if (response.data && response.data.audioContent) {
+      const audio = new Audio(response.data.audioContent);
+    }
     const url = `/.netlify/functions/tts?text=${encodeURIComponent(text)}`;
 
     // Create a new Audio object and play it
-    const audio = new Audio(url);
+    const audio = new Audio(response.data.audioContent);
 
     audio.play().catch((error) => {
       console.error("Audio playback failed:", error);
